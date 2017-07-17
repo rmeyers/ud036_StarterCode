dffbc277fe26e3af00f52bb41509cd8b8d31ea69
Ryan Meyers <ryan.d.meyers@gmail.com>
2017-07-17 17:13:32 -0400

    Added comments

--- media.py
+++ media.py
@@ -5,7 +5,3 @@ class Movie():
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        import webbrowser
-        webbrowser.open(self.trailer_youtube_url)
