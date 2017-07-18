82d40c65bb65482f4b51a8ae036d4185dbfa30c2
Ryan Meyers <ryan.d.meyers@gmail.com>
2017-07-18 09:58:32 -0400

    Adhering to PEP8 styles and adding one comment for resubmission

--- media.py
+++ media.py
@@ -1,7 +1,12 @@
 
 class Movie():
-    ''' This creates a new movie instance. '''
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    ''' This creates a new movie instance, including variables for
+    movie title, poster_image, and YouTube link. '''
+    def __init__(
+        self, movie_title, movie_storyline, poster_image,
+        trailer_youtube
+    ):
+        # Here we create the self variables from the function inputs.
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
